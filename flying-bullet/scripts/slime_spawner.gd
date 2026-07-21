9b51fb434a4cf6bf942468f211df04463870fe06
taumois <238574759+taumois@users.noreply.github.com>
2026-07-22 11:53:15 +1200

    I

--- flying-bullet/scripts/slime_spawner.gd
+++ flying-bullet/scripts/slime_spawner.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	bullet = get_node("../Bullet")
 
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	var new_slime = LASER_SLIME.instantiate() if randf() > 0.5 else ROCKET_SLIME.instantiate()
 	var offset_vector = Vector2.from_angle(randf() * PI) * DISTANCE_TO_SPAWN_FROM_BULLET
 	new_slime.position = bullet.position + offset_vector
